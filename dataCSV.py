946437def102bb7738b986b2d5abaae261226012
JulioHolanda <juliophc@msn.com>
2022-05-19 17:34:32 -0300

    CPF retirado

--- dataCSV.py
+++ dataCSV.py
@@ -3,38 +3,62 @@ import csv
 with open('projetos1-g12/studentData.csv', 'w', newline='') as data_file:
     writer = csv.writer(data_file, delimiter=',')
     
-    writer.writerow(["NOME","CPF","MATRICULA","TURMA","NOTA1","NOTA2"])
+    writer.writerow(["NOME","MATRICULA","TURMA","NOTA1","NOTA2"])
     writer.writerow([
         "JULIA",
-        "100.240.224-69",
         203040,
-        "#5B",
+        "5B",
         7.0,
         6.5
     ])
     writer.writerow([
         "MATEUS",
-        "100.240.000-33",
         102030,
-        "#5A",
+        "5A",
         6.5,
         8.0
     ])
     writer.writerow([
         "DANILO",
-        "100.240.111-47",
         605070,
-        "#7C",
+        "7C",
         9.5,
         4.0
     ])
     writer.writerow([
         "GUILHERME",
-        "20.294.224-17",
         6666666,
-        "#7A",
+        "7A",
         7.5,
         5.0
     ])
-    
+    writer.writerow([
+        "CADU",
+        454545,
+        "7A",
+        3.0,
+        10.0
+    ])
+    writer.writerow([
+        "RODRIGO",
+        505060,
+        "7B",
+        7.5,
+        8.5
+    ])
+    writer.writerow([
+        "JOSE",
+        334453,
+        "5A",
+        8.5,
+        9.0
+    ])
+    writer.writerow([
+        "ROSA",
+        551223,
+        "5B",
+        6.0,
+        8.5
+    ])
+
     data_file.close()
--- funcoes.py
+++ funcoes.py
@@ -6,23 +6,21 @@ def media(valor1, valor2):
 
 def pergunta_filtro():
     print("Deseja procurar por:")
-    print("Nome [0]\nCPF [1]")
-    print("Matrícula [2]\nTurma [3]")
-    print("Nota 1 [4]\nNota 2 [5]")
+    print("Nome [0]")
+    print("Matrícula [1]\nTurma [2]")
+    print("Nota 1 [3]\nNota 2 [4]")
     
 
 def resp_filtro(x):
     if x == 0:
-        resp = input("Insira o nome: ").upper()
+        resp = input("Insira o nome: ").upper()    
     elif x == 1:
-        resp = input("insira o CPF: ")
-    elif x == 2:
         resp = input("insira o número da matrícula: ")
+    elif x == 2:
+        resp = input("insira a turma: ").upper()
     elif x == 3:
-        resp = input("insira a turma: ")
-    elif x == 4:
         resp = input("insira a NOTA 1 (com casa decimal): ")
-    elif x == 5:
+    elif x == 4:
         resp = input("insira a Nota 2 (com casa decimal): ")
     return resp
 
@@ -34,7 +32,7 @@ def procura():
     i = int(input())
     print()
 
-    while i<0 and i>5:
+    while i<0 and i>4:
         print("\nINSIRA UM VALOR VÁLIDO\n")
         pergunta_filtro()
         i = int(input())
@@ -59,9 +57,9 @@ def procura():
     print()
 
 # addstd = add student
-def addstd(nome,cpf,matricula,turma,nota1,nota2):
+def addstd(nome,matricula,turma,nota1,nota2):
     
-    list = [nome,cpf,matricula,turma,nota1,nota2]
+    list = [nome,matricula,turma,nota1,nota2]
 
     with open('projetos1-g12/studentData.csv', 'a') as data_adder:
         writer_object = csv.writer(data_adder)
@@ -69,12 +67,12 @@ def addstd(nome,cpf,matricula,turma,nota1,nota2):
         
         data_adder.close()
 
-def novos_dados():
+def novos_d():
     print("Insira os dados do novo estudante:")
     nome = input("NOME: ").upper()
-    cpf = input("CPF: ")
     matrc = int(input("MATRÍCULA: "))
-    turma = input("TURMA: ")
+    turma = input("TURMA: ").upper()
     nota1 = float(input("1ª nota: "))
     nota2 = float(input("2ª nota: "))
-    addstd(nome,cpf,matrc,turma,nota1,nota2)
+    addstd(nome,matrc,turma,nota1,nota2)
+
